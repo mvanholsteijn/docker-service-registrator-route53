f05dc0f42286ed4daa99a3f2281dc70ae657883d
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2017-08-13 12:52:37 +0200

    include synchronisation on startup

--- ecssd.py
+++ ecssd.py
@@ -7,7 +7,7 @@ import boto3
 import socket
 from botocore.exceptions import ClientError
 
-logging.basicConfig()
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('ECSServiceRegistrator')
 log.setLevel('INFO')
 
@@ -29,8 +29,6 @@ class ECSServiceRegistrator:
             try:
                 response = self.route53.get_hosted_zone(Id=hosted_zone_id)
                 self.dns_name = response['HostedZone']['Name']
-                log.info('found dns name %s for hosted zone id %s' %
-                         (self.dns_name, self.hosted_zone_id))
             except ClientError as e:
                 raise click.UsageError('%s' % e)
         else:
@@ -52,6 +50,8 @@ class ECSServiceRegistrator:
                     'There is no hosted zones for the DNS name %s' % self.dns_name)
 
         assert self.dns_name is not None and self.hosted_zone_id is not None
+        log.info('found dns name %s for hosted zone id %s' %
+                 (self.dns_name, self.hosted_zone_id))
 
     def get_environment_of_container(self, container):
         result = {}
@@ -71,8 +71,6 @@ class ECSServiceRegistrator:
         for page in page_iterator:
             self.srv_records.extend(filter(
                 lambda rr: self.is_managed_resource_record_set(rr), page['ResourceRecordSets']))
-        log.info('found %d SRV records for host %s' %
-                 (len(self.srv_records), self.hostname))
 
     def create_service_record(self, container_id, port, name):
         return {
@@ -88,21 +86,16 @@ class ECSServiceRegistrator:
             "SetIdentifier": "%s:%s" % (self.hostname, container_id)
         }
 
-    def register_service(self, container_id, port, name):
-        log.warn('registering port %s of container %s with service name %s' %
-                 (port, container_id, name))
-
-        set_id = '%s:%s' % (self.hostname, container_id)
+    def register_service(self, service_records):
         try:
-            record = self.create_service_record(container_id, port, name)
-            self.route53.change_resource_record_sets(HostedZoneId=self.hosted_zone_id,
-                                                     ChangeBatch={"Changes": [{"Action": "UPSERT", "ResourceRecordSet": record}]})
-            r = self.find_srv_records(container_id)
-            self.update_srv_record(record)
-
+            batch = {"Changes": [
+                {"Action": "UPSERT", "ResourceRecordSet": r} for r in service_records]}
+            self.route53.change_resource_record_sets(
+                HostedZoneId=self.hosted_zone_id, ChangeBatch=batch)
+            for r in service_records:
+                self.update_srv_record(r)
         except ClientError as e:
-            log.error('could not add SRV record for container %s, %s' %
-                      (container_id, e))
+            log.error('could not add SRV records, %s' % (container_id, e))
 
     def update_srv_record(self, record):
         for i, rr in enumerate(self.srv_records):
@@ -118,8 +111,8 @@ class ECSServiceRegistrator:
     def deregister_service(self, container_id):
         records = self.find_srv_records(container_id)
         if len(records) > 0:
-            log.info('deregistering container %s with srv records %s' % (
-                container_id, ' '.join(map(lambda r: r['Name'], records))))
+            log.info('deregistering %d SRV records for container %s' %
+                     (len(records), container_id))
 
             try:
                 batch = {"Changes": [
@@ -130,40 +123,111 @@ class ECSServiceRegistrator:
                 log.error('could not remove %d SRV records for container %s, %s' %
                           (len(records), container_id, e))
         else:
-            log.warn('No SRV record found for container with id %s' %
-                     container_id)
+            log.debug('No SRV record found for container with id %s' %
+                      container_id)
+
+    def create_service_records(self, container):
+        result = []
+        env = self.get_environment_of_container(container)
+        ports = container.attrs['NetworkSettings']['Ports']
+
+        for port in ports:
+            if ports[port] is None:
+                # no ports exposed
+                continue
+
+            hostPort = ports[port][0]['HostPort']
+            service_name = 'SERVICE_%s_NAME' % port.split('/')[0]
+            if service_name in env:
+                result.append(self.create_service_record(
+                    container.attrs['Id'], hostPort, env[service_name]))
+            elif 'SERVICE_NAME' in env and len(ports) == 1:
+                result.append(self.create_service_record(
+                    container.attrs['Id'], hostPort, env['SERVICE_NAME']))
+            else:
+                pass
+
+        return result
 
     def container_started(self, container_id, event):
         try:
             container = self.dockr.containers.get(container_id)
-            env = self.get_environment_of_container(container)
-            ports = container.attrs['NetworkSettings']['Ports']
+            service_records = self.create_service_records(container)
+            if len(service_records) > 0:
+                log.info('registering %d SRV record for container %s' %
+                         (len(service_records), container_id))
+                self.register_service(service_records)
 
-            for p in ports:
-                if ports[p] is None:
-                    log.debug('port %s of container %s is not exposed' %
-                              (p, container_id))
-                    continue
-
-                port = p.split('/')
-                hostPort = ports[p][0]['HostPort']
-                service_name = 'SERVICE_%s_NAME' % port[0]
-                if service_name in env:
-                    self.register_service(
-                        container_id, hostPort, env[service_name])
-                elif 'SERVICE_NAME' in env and len(ports) == 1:
-                    self.register_service(
-                        container_id, hostPort, env['SERVICE_NAME'])
-                else:
-                    log.warn('port %s of container %s is exposed but ignored' % (
-                        p, container_id))
         except docker.errors.NotFound as e:
             log.error('container %s does not exist.' % container_id)
 
     def container_died(self, container_id, event):
         self.deregister_service(container_id)
 
+    def deregister_orphaned_records(self, set_ids):
+        records = filter(lambda r: r['SetIdentifier']
+                         in set_ids, self.srv_records)
+        if len(records) > 0:
+            log.info('deregistering srv records %s' % (
+                ' '.join(map(lambda r: r['Name'], records))))
+
+            try:
+                batch = {"Changes": [
+                    {"Action": "DELETE", "ResourceRecordSet": rr} for rr in records]}
+                self.route53.change_resource_record_sets(
+                    HostedZoneId=self.hosted_zone_id, ChangeBatch=batch)
+                self.srv_re = filter(
+                    lambda r: r['SetIdentifier'] not in set_ids, self.srv_records)
+
+            except ClientError as e:
+                log.error('could not remove %d SRV records, %s' %
+                          (len(records), container_id, e))
+
+    def register_running_containers(self, containers):
+        records = []
+        for c in containers:
+            records.extend(self.create_service_records(c))
+
+        if len(records) > 0:
+            log.info('registering srv records %s' % (
+                ' '.join(map(lambda r: r['Name'], records))))
+
+            try:
+                batch = {"Changes": [
+                    {"Action": "UPSERT", "ResourceRecordSet": rr} for rr in records]}
+                self.route53.change_resource_record_sets(
+                    HostedZoneId=self.hosted_zone_id, ChangeBatch=batch)
+                self.srv_records.extend(records)
+
+            except ClientError as e:
+                log.error('could not add %d SRV records, %s' %
+                          (len(records), e))
+
+    def sync(self):
+        self.load_hosted_zone()
+        containers = self.dockr.containers.list()
+        in_zone = set(map(lambda rr: rr['SetIdentifier'], self.srv_records))
+        running = set(map(lambda c: '%s:%s' %
+                          (self.hostname, c.attrs['Id']), containers))
+        to_delete = in_zone - running
+        to_add = running - in_zone
+
+        if len(to_delete) > 0 or len(to_add) > 0:
+            log.info('zone for host %s out of sync, adding %d and removing %d records' % (
+                self.hostname, len(to_add), len(to_delete)))
+
+            self.deregister_orphaned_records(to_delete)
+
+            containers = filter(lambda c: '%s:%s' % (
+                self.hostname, c.attrs['Id']) in to_add, containers)
+            self.register_running_containers(containers)
+        else:
+            log.info('zone for host %s in sync', self.hostname)
+
     def process_events(self):
+        self.load_hosted_zone()
+        log.info('found %d SRV records for host %s' %
+                 (len(self.srv_records), self.hostname))
         for e in self.dockr.events():
             lines = filter(lambda l: len(l) > 0, e.split('\n'))
             for line in lines:
@@ -177,9 +241,6 @@ class ECSServiceRegistrator:
                         log.debug('skipping event %s' % event['status'])
                 else:
                     pass  # boring...
-
-    def main(self):
-        self.process_events()
 
 
 @click.group()
@@ -201,8 +262,16 @@ def cli(ctx, dns_name, hosted_zone_id, hostname):
 def run(ctx):
     e = ECSServiceRegistrator(ctx.obj['dns_name'], ctx.obj[
                               'hosted_zone_id'], ctx.obj['hostname'])
-    e.load_hosted_zone()
+    e.sync()
     e.process_events()
+
+
+@cli.command()
+@click.pass_context
+def sync(ctx):
+    e = ECSServiceRegistrator(ctx.obj['dns_name'], ctx.obj[
+                              'hosted_zone_id'], ctx.obj['hostname'])
+    e.sync()
 
 if __name__ == '__main__':
     cli(obj={})
