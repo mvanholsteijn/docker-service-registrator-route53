0b641a747409431c5d90c947a1464ec264d220f6
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2017-08-14 09:25:45 +0200

    added remove_all, started documentation

--- ecssd.py
+++ ecssd.py
@@ -1,3 +1,21 @@
+"""
+manages the SRV records for Docker containers running on a specific host.
+When a container is start, the registrator will create a SRV record for each
+exposed port which has a matching SERVICE_<exposed-port>_NAME environment
+variable. If the container exposes a single port, it suffices to have a SERVICE_NAME
+environment variable.
+
+The registrator will create SRV records in Route53 with `<hostname>:<container-id>`
+as set identifier. This allows the registrator to synchronise SRV records with
+the current state of the running instances on the host. 
+
+The registrator has three commands: 'remove_all', 'sync' and 'run'.
+    
+    remove_all  - remove all service records point to this host
+    sync        - synchronise the service records with the running containers
+    run         - continuously update the SRV records by subscribing to the Docker event stream
+
+"""
 import sys
 import json
 import logging
@@ -13,47 +31,98 @@ log.setLevel('INFO')
 
 
 class ECSServiceRegistrator:
-
+    """
+    manages SRV record registrations in Route53 for docker containers
+    """
     def __init__(self, dns_name, hosted_zone_id, hostname):
+        """
+        constructor. determines the Route53 hosted zone by either the dns_name or the hosted_zone_id.
+        requires:
+        - hostname is not None
+        - valid hosted_zone_id is hosted_zone_id is not None
+        - dns_name exists in Route53
+        ensures:
+        - self.dns_name == dns_name if dns_name is not None and hosted_zone_id is None 
+        - self.hosted_zone_id == hosted_zone_id if hosted_zone_id is not None
+        - self.hostname == hostname
+        """
+        assert hostname is not None
+        assert hosted_zone_id is not None or dns_name is not None
+
         self.dockr = docker.from_env()
         self.route53 = boto3.client('route53')
 
         self.hostname = hostname
-        self.complete_hosted_zone_info(dns_name, hosted_zone_id)
-
-    def complete_hosted_zone_info(self, dns_name, hosted_zone_id):
-        assert dns_name is not None or hosted_zone_id is not None
 
         if hosted_zone_id is not None:
-            self.hosted_zone_id = hosted_zone_id
-            try:
-                response = self.route53.get_hosted_zone(Id=hosted_zone_id)
-                self.dns_name = response['HostedZone']['Name']
-            except ClientError as e:
-                raise click.UsageError('%s' % e)
+            self.get_dns_name_for_hosted_zone_id(hosted_zone_id)
         else:
-            self.dns_name = dns_name if dns_name[
-                -1] == '.' else '%s.' % dns_name
-            response = self.route53.list_hosted_zones_by_name(
-                DNSName=self.dns_name)
-            zones = filter(lambda r: r['Name'] ==
-                           self.dns_name, response['HostedZones'])
-            if len(zones) == 1:
-                self.hosted_zone_id = zones[0]['Id'].split('/')[-1]
-                log.warn('found hosted zone id %s for dns name %s' %
-                         (self.hosted_zone_id, self.dns_name))
-            elif len(zones) > 1:
-                raise click.UsageError(
-                    'There are %d hosted zones for the DNS name %s, please specify --hosted-zone-id' % (len(zones), self.dns_name))
-            else:
-                raise click.UsageError(
-                    'There is no hosted zones for the DNS name %s' % self.dns_name)
+            self.get_hosted_zone_id_for_dns_name(dns_name)
+        
+        assert self.hostname is not None
+        assert self.dns_name is not None
+        assert self.hosted_zone_id is not None
 
-        assert self.dns_name is not None and self.hosted_zone_id is not None
+
+    def get_dns_name_for_hosted_zone_id(self, hosted_zone_id):
+        """
+        gets the dns name associated with the 'hosted_zone_id'.
+        ensure:
+            self.hosted_zone_id == hosted_zone_id
+            self.dns_name[-1] == '.'
+        """
+        assert hosted_zone_id is not None
+        self.hosted_zone_id = hosted_zone_id
+        try:
+            response = self.route53.get_hosted_zone(Id=hosted_zone_id)
+            self.dns_name = response['HostedZone']['Name']
+        except ClientError as e:
+            raise click.UsageError('%s' % e)
+
+        log.info('found hosted zone %s for dns name %s' % (self.hosted_zone_id, self.dns_name))
+
+        assert self.dns_name[-1] == '.'
+        assert self.hosted_zone_id is not None
+
+
+    def get_hosted_zone_id_for_dns_name(self, dns_name):
+        """
+        gets the hosted_zone_id associated with the 'dns_name'. 
+        require:
+            there is exactly 1 hosted zone for  'dns_name' in Route53.
+        ensure:
+            self.hosted_zone_id == hosted_zone_id
+            self.dns_name[-1] == '.'
+            self.dns_name[-1] == dns_name
+        """
+        self.dns_name = dns_name if dns_name[-1] == '.' else '%s.' % dns_name
+        response = self.route53.list_hosted_zones_by_name(DNSName=self.dns_name)
+        zones = filter(lambda r: r['Name'] == self.dns_name, response['HostedZones'])
+        if len(zones) == 1:
+            self.hosted_zone_id = zones[0]['Id'].split('/')[-1]
+            log.warn('found hosted zone id %s for dns name %s' % (self.hosted_zone_id, self.dns_name))
+        elif len(zones) > 1:
+            raise click.UsageError('There are %d hosted zones for the DNS name %s, please specify --hosted-zone-id' % (len(zones), self.dns_name))
+        else:
+            raise click.UsageError('There is no hosted zones for the DNS name %s' % self.dns_name)
+
+        assert self.dns_name[-1] == '.'
+        assert self.hosted_zone_id is not None
+
         log.info('found dns name %s for hosted zone id %s' %
                  (self.dns_name, self.hosted_zone_id))
 
     def get_environment_of_container(self, container):
+        """
+        require:
+            container is not None
+            attrs in container 
+            'Config' in container.attrs
+            'Env' in container.attrs['Config']['Env']
+        ensure:
+            isinstance(result, dict)
+            len(dict) == len(container.attrs['Config']['Env'])
+        """
         result = {}
         env = container.attrs['Config']['Env']
         for e in env:
@@ -62,6 +131,15 @@ class ECSServiceRegistrator:
         return result
 
     def is_managed_resource_record_set(self, rr):
+        """
+        returns true if the resource record is managed by this hostname.
+        require:
+            rr is not None
+            'Type' in rr
+            'SetIdentifier' in rr
+        ensure:
+            result == (rr['Type'] == 'SRV' and rr['SetIdentifier'].startswith('%s:' % self.hostname)
+        """
         return rr['Type'] == 'SRV' and 'SetIdentifier' in rr and rr['SetIdentifier'].startswith('%s:' % self.hostname)
 
     def load_hosted_zone(self):
@@ -164,9 +242,7 @@ class ECSServiceRegistrator:
     def container_died(self, container_id, event):
         self.deregister_service(container_id)
 
-    def deregister_orphaned_records(self, set_ids):
-        records = filter(lambda r: r['SetIdentifier']
-                         in set_ids, self.srv_records)
+    def deregister_orphaned_records(self, records):
         if len(records) > 0:
             log.info('deregistering srv records %s' % (
                 ' '.join(map(lambda r: r['Name'], records))))
@@ -176,12 +252,12 @@ class ECSServiceRegistrator:
                     {"Action": "DELETE", "ResourceRecordSet": rr} for rr in records]}
                 self.route53.change_resource_record_sets(
                     HostedZoneId=self.hosted_zone_id, ChangeBatch=batch)
-                self.srv_re = filter(
+                set_ids = set(map(lambda r: r['SetIdentifier'], records))
+                self.srv_records = filter(
                     lambda r: r['SetIdentifier'] not in set_ids, self.srv_records)
 
             except ClientError as e:
-                log.error('could not remove %d SRV records, %s' %
-                          (len(records), container_id, e))
+                log.error('could not remove %d SRV records, %s' % (len(records), e))
 
     def register_running_containers(self, containers):
         records = []
@@ -215,11 +291,10 @@ class ECSServiceRegistrator:
         if len(to_delete) > 0 or len(to_add) > 0:
             log.info('zone for host %s out of sync, adding %d and removing %d records' % (
                 self.hostname, len(to_add), len(to_delete)))
+            records = filter(lambda r: r['SetIdentifier'] in to_delete, self.srv_records)
+            self.deregister_orphaned_records(records)
 
-            self.deregister_orphaned_records(to_delete)
-
-            containers = filter(lambda c: '%s:%s' % (
-                self.hostname, c.attrs['Id']) in to_add, containers)
+            containers = filter(lambda c: '%s:%s' % (self.hostname, c.attrs['Id']) in to_add, containers)
             self.register_running_containers(containers)
         else:
             log.info('zone for host %s in sync', self.hostname)
@@ -242,7 +317,10 @@ class ECSServiceRegistrator:
                 else:
                     pass  # boring...
 
-
+    def remove_all(self):
+        self.load_hosted_zone()
+        self.deregister_orphaned_records(self.srv_records)
+    
 @click.group()
 @click.option('--dns-name', required=False, help='to synchronize the SRV records with.')
 @click.option('--hosted-zone-id', required=False, help='to synchronize the SRV records with.')
@@ -265,6 +343,12 @@ def run(ctx):
     e.sync()
     e.process_events()
 
+@cli.command()
+@click.pass_context
+def remove_all(ctx):
+    e = ECSServiceRegistrator(ctx.obj['dns_name'], ctx.obj[
+                              'hosted_zone_id'], ctx.obj['hostname'])
+    e.remove_all()
 
 @cli.command()
 @click.pass_context
